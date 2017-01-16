727373cf92cca721fdd2285675701d58565b1bb4
wcy <wcy@travelsky.com>
2017-01-16 17:15:28 +0800

    update

--- js/articles.js
+++ js/articles.js
@@ -5,7 +5,7 @@ $(function(){
   $('.logo').on('click',function(){
     location.href='../index.html';
   })
-  
+
   $('.weibo').on('click',function(){
     window.open("http://weibo.com/miracleuk");
   })
@@ -16,29 +16,16 @@ $(function(){
   $('.mask').on('click',function(){
     $(this).hide();
   })
-  // $('.pure-input-1-2').on('change',function(){
-  //     switch ($(this).val()) {
-  //       case 'Education':
-  //         location.href='../dropdown/universities.html';
-  //         break;
-  //       case 'Property':
-  //         location.href='../dropdown/Apartment.html';
-  //         break;
-  //       case 'Team':
-  //         location.href='../dropdown/Team.html';
-  //         break;
-  //         case 'HND&Colleges':
-  //           location.href='../dropdown/hn&College.html';
-  //           break;
-  //         case 'Alumni Association':
-  //           location.href='../dropdown/alumniassociation.html';
-  //           break;
-  //         case'Accommodation':
-  //           location.href='../dropdown/accommodation.html';
-  //           break;
-  //       default:
-  //           return false;
-  //
-  //     }
-  // })
+var contentHeight = $('.content').height();
+var rectHeight = $('html').css('font-size').replace('px','')*3;
+$('.animate-letters').append(
+  '<div class="animate-letter"></div>'
+)
+for(var i=0;i<Math.ceil(contentHeight/rectHeight);i++){
+  $('.animate-letters').append(
+    '<div class="animate-letter"></div>'
+  )
+
+}
+
 })
